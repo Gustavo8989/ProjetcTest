d622d52ea4b82849bc185d41b43b439445d2f4f6
Gustavo <gustavohenriquealves418@gmail.com>
2026-02-19 21:52:03 -0300

    sistema para calcular preco de compra

---
+++ preco.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+
+
+#df = pd.read_excel('teste.xlsl')
+#df['Preço alvo'] = df['valores'] * 0.05 
+
+teste = 10 
+porcentagem = teste * 0.05
+teste_porcen = teste - porcentagem
+print(teste_porcen)
