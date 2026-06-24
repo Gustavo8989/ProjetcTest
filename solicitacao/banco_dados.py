e54dd91d26b2d356ebb0831e0c1f7e8941b26487
Gustavo <gustavohenriquealves418@gmail.com>
2026-06-23 21:46:47 -0300

    Tela de login

--- solicitacao/banco_dados.py
+++ solicitacao/banco_dados.py
@@ -5,6 +5,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+# Função para postar dados no banco de dados
 @app.route("/salvar", methods=["POST"])
 def salvar_dados():
     dados = request.get_json()
@@ -29,6 +30,34 @@ def salvar_dados():
     conexao.commit()
     conexao.close()
     return jsonify({"mensagem":"Conta criado com sucesso"})
+
+
+#Função para pegar os dados
+@app.route("/contas", methods=["POST"])
+def get_data():
+    dados = request.get_json()
+    email = dados['email']
+    senha = dados['senha']
+    conexcao = sqlite3.connect("requis.db")
+    cursor = conexcao.cursor()
+    cursor.execute("SELECT * FROM contas WHERE email = ?",(email,))
+    resultado = cursor.fetchall()
+    if not resultado :
+        return jsonify({
+            'Sucesso':False,
+            'Mensagem':'Email não encontrado'})
+    elif resultado[0] != senha:
+        return jsonify({
+            'Sucesso':False,
+            'Mensagem':'Senha incorreta'
+        })
+    conexcao.close()
+    # Pegar os dados que foram colocados no login comparar se é compativel com alguma senha salva no banco de dados logo após exluir
+    return jsonify({
+        "Sucesso": True,
+        'Mensagem':'Login realizado com sucesso'
+    })
+
 
 app.run(debug=True)
 # A segurança das contas vai utilizar um sistema basico hash de senha
